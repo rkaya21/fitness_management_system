8ce76ac3755239cf7b07ef5216c1067c437c128a
Recep Enes Kaya <116019903+rkaya21@users.noreply.github.com>
2024-05-17 11:38:39 +0300

    create the main window

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-# empty
+import tkinter as tk
+from tkinter import messagebox
+from database import Database
+
+class FitnessApp:
+    def __init__(self, root):
+        self.db = Database()
+
+        self.root = root # Create the main window.
+        self.root.title("Fitness Management System")
+
+        self.create_widgets() # Create the interface components.
+
