45e0347b16b1ad3dc72a4e6c4853736f1177f2db
Recep Enes Kaya <116019903+rkaya21@users.noreply.github.com>
2024-05-17 14:14:12 +0300

    adding a member,displaying members updating a member and deleting a member have been written

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ class Database:
         self.cursor = self.conn.cursor
         self.create_table()
 
+
     def create_table(self):
         self.cursor.execute(''' CREATE TABLE IF NOT EXIST members
                             (id INTEGER PRIMARY KEY,
@@ -14,30 +15,61 @@ class Database:
                             age INTEGER,
                             city TEXT,
                             price REAL,
-                            membership_duration INTEGER''')
-
+                            membership_duration INTEGER''') # Adding columns to members.
         self.conn.commit()
 
-    def add_member(self):
-        # Method to add a new member.
-        pass
+
+    def add_member(self, name, last_name, age, city, price, membership_duration):
+        """
+        Method to adds a member.
+        :param name:
+        :param last_name:
+        :param age:
+        :param city:
+        :param price:
+        :param membership_duration:
+        :return:
+        """
+        self.cursor.execute(
+            "INSERT INTO members (name, last_name, age, city, price, membership_duration"
+            "VALUES (?, ?, ?, ?, ?, ?)",
+            name, last_name, age, city, price, membership_duration)
+        self.conn.commit()
 
 
     def get_members(self):
-        # Method to existing members.
-        pass
+        """
+        Method to show members.
+        """
+        self.cursor.execute("SELECT * FROM members")
+        return self.cursor.fetchall()
 
 
-    def update_member(self):
-        # Method to update a member's information.
-        pass
+    def update_member(self, name, last_name, age, city, price, membership_duration):
+        """
+        Method to update a member's information.
+        :param name:
+        :param last_name:
+        :param age:
+        :param city:
+        :param price:
+        :param membership_duration:
+        :return:
+        """
+        self.cursor.execute(
+            "UPDATE members SET name=?, last_name=?, age=?, city=?, price=?, membership_duration=?"
+            "WHERE id=?",
+            name, last_name, age, city, price, membership_duration)
+        self.conn.commit()
 
 
-    def delete_member(self):
-        # Method to delete a member.
-        pass
-
-
-    def __del__(self):
-        # destructor
-        self.conn.close()
+    def delete_member(self, member_id):
+        """
+        Method to delete a member
+        :param member_id:
+        :return:
+        """
+        self.cursor.execute("DELETE FROM members"
+                            "WHERE id=?",
+                            (member_id,))
+        self.conn.commit()
